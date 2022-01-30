df498b1a9f1241bdc4460bc68cdc37d0abdfeaa0
Marcos Corsi <marcosvcorsi@gmail.com>
2022-01-30 15:21:17 -0300

    test: refactor integration tests

--- test/users.test.ts
+++ test/users.test.ts
@@ -11,17 +11,46 @@ describe("Users Integration", () => {
     await prismaClient.$connect();
   });
 
+  beforeEach(async () => {
+    await prismaClient.user.deleteMany();
+  });
+
   afterAll(async () => {
     await prismaClient.$disconnect();
   });
 
-  it("should return users", async () => {
+  it("should return users data", async () => {
+    const user = await prismaClient.user.create({
+      data: {
+        email: "anymail@mail.com",
+        name: "any_name",
+      },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+      },
+    });
+
     const response = await request(app).post("/graphql").send({
       query: "{ users { id, email, name } }",
     });
 
-    console.log(response.body);
+    expect(response.body.data.users).toEqual([user]);
+  });
 
-    expect(true).toBe(true);
+  it("should create user", async () => {
+    const params = {
+      email: "anymail@mail.com",
+      name: "any_name",
+    };
+
+    const response = await request(app)
+      .post("/graphql")
+      .send({
+        query: `mutation { createUser(input: { email: "${params.email}", name: "${params.name}" }) { id, email, name } }`,
+      });
+
+    expect(response.body.data.createUser).toMatchObject(params);
   });
 });
