fc07e15162bcb96aa5573b4db09c32cf6bd33d9d
Marcos Corsi <marcosvcorsi@gmail.com>
2022-01-30 13:51:28 -0300

    feat: add mutation in memory

--- src/graphql/resolvers/users.ts
+++ src/graphql/resolvers/users.ts
@@ -1,16 +1,31 @@
-import { Query, Resolver } from "type-graphql";
-import { User } from "../schemas/user";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { v4 } from "uuid";
+import { CreateUserInput, User } from "../schemas/user";
 
 @Resolver()
 export class UsersResolver {
+  userList: User[];
+
+  constructor() {
+    this.userList = [];
+  }
+
   @Query(() => [User])
   async users(): Promise<User[]> {
-    return [
-      {
-        id: "1",
-        email: "test@test.com",
-        name: "Test",
-      },
-    ];
+    return this.userList;
+  }
+
+  @Mutation(() => User)
+  async createUser(@Arg("input") input: CreateUserInput): Promise<User> {
+    const id = v4();
+
+    const user = {
+      id,
+      ...input,
+    };
+
+    this.userList.push(user);
+
+    return user;
   }
 }
--- src/graphql/schemas/user.ts
+++ src/graphql/schemas/user.ts
@@ -1,10 +1,19 @@
-import { Field, ID, ObjectType } from "type-graphql";
+import { Field, ID, InputType, ObjectType } from "type-graphql";
 
 @ObjectType()
 export class User {
   @Field(() => ID)
   id!: string;
 
+  @Field()
+  email!: string;
+
+  @Field()
+  name!: string;
+}
+
+@InputType()
+export class CreateUserInput {
   @Field()
   email!: string;
 
