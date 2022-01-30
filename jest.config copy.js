0d1333a03eb1d18dc239420774eda80fad581aba
Marcos Corsi <marcosvcorsi@gmail.com>
2022-01-30 15:07:01 -0300

    chore: setup integration tests

--- jest.config copy.js
+++ jest.config copy.js
@@ -146,7 +146,10 @@ module.exports = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  testMatch: ['**/*.spec.ts'],
+  // testMatch: [
+  //   "**/__tests__/**/*.[jt]s?(x)",
+  //   "**/?(*.)+(spec|test).[tj]s?(x)"
+  // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
--- test/users.test.ts
+++ test/users.test.ts
@@ -1,7 +1,20 @@
+import { PrismaClient } from "@prisma/client";
 import request from "supertest";
 import { app } from "../src/app";
 
 describe("Users Integration", () => {
+  let prismaClient: PrismaClient;
+
+  beforeAll(async () => {
+    prismaClient = new PrismaClient();
+
+    await prismaClient.$connect();
+  });
+
+  afterAll(async () => {
+    await prismaClient.$disconnect();
+  });
+
   it("should return users", async () => {
     const response = await request(app).post("/graphql").send({
       query: "{ users { id, email, name } }",
